613cfa98a56fc55035088a82658ea1a890683c3e
“Chavda <chavdasandip2908@gmail.com>
2025-03-23 23:05:49 +0530

    complet the send code fuature

--- js/forgot-pass.js
+++ js/forgot-pass.js
@@ -1,5 +1,6 @@
 async function sendOtp() {
     let email = document.getElementById("email").value;
+
     try {
         const response = await fetch(`${BACKEND_URL}/api/v1/auth/forgot-password`, {
             method: "POST",
@@ -8,9 +9,18 @@ async function sendOtp() {
             },
             body: JSON.stringify({ email })
         });
-    }
-    catch (error) {
-        console.log(error)
+
+        if (response.ok) {
+            document.getElementById("email-field").style.display = "none"; // Hide email field
+            document.getElementById("new-pass-field").style.display = "block"; // Show new password field
+        } else {
+            console.log(response);
+            document.getElementById("email-message").innerText = "Unable to send CODE";
+            console.log("Error: Unable to send CODE");
+        }
+    } catch (error) {
+        document.getElementById("email-message").innerText = error.message;
+        console.log("Fetch error:", error);
     }
 }
 
