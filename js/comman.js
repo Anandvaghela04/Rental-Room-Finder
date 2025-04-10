740c87f5ed543e15eadad7d87bbc69cf950c2bbf
“Chavda <chavdasandip2908@gmail.com>
2025-04-10 12:39:09 +0530

    change url

--- js/comman.js
+++ js/comman.js
@@ -1,7 +1,7 @@
 // common.js
 // const BACKEND_URL = "http://127.0.0.1:5000";
-// const BACKEND_URL = "https://rental-room-finder-server.onrender.com";
-const BACKEND_URL = "https://rental-room-finder-server-odzy.onrender.com";
+const BACKEND_URL = "https://rental-room-finder-server.onrender.com";
+// const BACKEND_URL = "https://rental-room-finder-server-odzy.onrender.com";
 
 // check in local storage for user login or not
 function checkUserLoggedIn() {
